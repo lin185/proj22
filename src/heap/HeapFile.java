26e415e154e5063227aacf919bcde28065d70ef7
ChiaHao Chen <chen1664@purdue.edu>
2015-03-07 17:15:53 -0500

    Add

--- src/heap/HeapFile.java
+++ src/heap/HeapFile.java
@@ -2,13 +2,36 @@ package heap;
 
 import chainexception.ChainException;
 import global.RID;
-
+import bufmgr.*;
+import global.*;
+import diskmgr.*;
 public class HeapFile {
-	public HeapFile(String name){}
+	HFPage rootHFPage;
+	PageId rootPageId;
+	public HeapFile(String name){
+		if((rootPageId= Minibase.DiskManager.get_file_entry(name) ) == null)	{ 
+		
+			Page newPage = new Page();
+			rootPageId = Minibase.BufferManager.newPage(newPage, 1);
+			rootHFPage = new HFPage(newPage);
+			rootHFPage.setPage(newPage);
+			rootHFPage.setCurPage(rootPageId);
+			PageId leftChild = new PageId(GlobalConst.INVALID_PAGEID);
+			PageId rightChild = new PageId(GlobalConst.INVALID_PAGEID);
+			rootHFPage.setNextPage(rightChild);
+			rootHFPage.setPrevPage(leftChild);
+			Minibase.DiskManager.add_file_entry(name, rootPageId);
+			Minibase.BufferManager.unpinPage(rootPageId, true);
+			
+		}
+	}
 	
 	public RID insertRecord(byte[] record) throws ChainException{
-		RID rid = null;
-		
+		RID rid = new RID();
+		PageId curPageId = new PageId(rootPageId.pid);
+		HFPage curHFPage = new HFPage();
+		Minibase.BufferManager.pinPage(rootPageId, rootHFPage, false);
+		if()
 		return rid;
 	}
 	
